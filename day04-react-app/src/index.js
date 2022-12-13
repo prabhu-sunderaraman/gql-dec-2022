326958d7a171ca7d3f27e5d40a462a4f5e9948ec
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2022-12-13 15:37:50 +0530

    Day04

--- day04-react-app/src/index.js
+++ day04-react-app/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloProvider} from '@apollo/client'
+import {apolloClient} from './client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+    
   </React.StrictMode>
 );
 
