c46eb00cfe5703c391b8ff37b98267454377b750
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2022-12-13 15:19:22 +0530

    Day04

--- day04-react-app/src/client.js
+++ day04-react-app/src/client.js
@@ -2,6 +2,11 @@ import  {ApolloClient, InMemoryCache} from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
     uri: "http://localhost:9000",
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    // defaultOptions: {
+    //     query: {
+    //         fetchPolicy: "no-cache"
+    //     }
+    // }
 }); 
 
